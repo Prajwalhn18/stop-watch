5cb81447f04fa4aaf461026ac2fcc5bc7bd01059
Prajwal H N <82883854+Prajwalhn18@users.noreply.github.com>
2022-01-31 18:47:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log('Js file connected');
+
+var count = 1;
+var minute = 0;
+var timer;
+
+const watchCountSeconds = document.getElementById('watch_count_seconds');
+const watchCountMinutes = document.getElementById('watch_count_minutes');
+const startButton = document.getElementById('watch_count_start');
+const stopButton = document.getElementById('watch_count_stop');
+const resetButton = document.getElementById('watch_count_reset');
+
+// Event listeners
+
+startButton.addEventListener('click', start);
+stopButton.addEventListener('click', stop);
+resetButton.addEventListener('click', resetCounter);
+
+//Application functions
+
+function start() {
+    timer = setInterval(() => {
+        let currentCount = count++;
+        setCount(currentCount);
+    }, 1000);
+
+    startButton.disabled = true;
+}
+
+function setCount(countNumber) {
+    if (countNumber != 0 && countNumber % 60 == 0) {
+        minute += 1;
+    }
+    watchCountMinutes.innerText = minute;
+    watchCountSeconds.innerText = countNumber;
+}
+
+function stop() {
+    startButton.disabled = false;
+    clearInterval(timer);
+    console.log('stopped');
+}
+
+function resetCounter() {
+    count = 0;
+    minute = 0;
+    setCount(0);
+}
